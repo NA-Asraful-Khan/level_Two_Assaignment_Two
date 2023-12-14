9dcb43fa083861675301da1cd1d2083bef70ac60
NA-Asraful-Khan <soyonshikhor@gmail.com>
2023-12-14 22:16:51 +0600

    Atlas connected and Port Fixed

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
 
+const PORT = config.port||8000
+
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`Example app listening on port ${PORT}`);
     });
   } catch (err) {
     console.log(err);
